2cfa5337b84b355d0fb50e9c8ee3c8e21c67540a
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-06-23 09:14:10 -0400

    mod to costmap file load; harcoded joystick device

--- autorally_util/setupEnvVariables.sh
+++ autorally_util/setupEnvVariables.sh
@@ -117,15 +117,18 @@ if [[ $DEBUG == true ]]; then
     echo "AR_LEFTCAM_CONNECTED  = ${AR_LEFTCAM_CONNECTED}"
 fi
 
-for device in /dev/input/js*; do
-    AR_JOYSTICK=$(udevadm info -n $device -q property --export | grep ID_INPUT_JOYSTICK)
-    AR_JOYSTICK=${AR_JOYSTICK#*=}
-    if [[ $AR_JOYSTICK == "'1'" ]]; then
-        export AR_JOYSTICK="$device"
-        break
-    fi
-    export AR_JOYSTICK=
-done
+## TODO:fix ››› unreliable joystick/yoke recognition
+#for device in /dev/input/js*; do
+#    AR_JOYSTICK=$(udevadm info -n $device -q property --export | grep ID_INPUT_JOYSTICK)
+#    AR_JOYSTICK=${AR_JOYSTICK#*=}
+#    if [[ $AR_JOYSTICK == "'1'" ]]; then
+#        export AR_JOYSTICK="$device"
+#        break
+#    fi
+#    export AR_JOYSTICK=
+#done
+export AR_JOYSTICK=/dev/input/js0  # Temp quick hack TODO:permanent solution
+
 
 YELLOW=`tput setaf 3`
 RESET=`tput sgr0`
