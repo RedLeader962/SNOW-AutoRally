c106ba153d8688246e49f47c76b5bf55356488a9
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-08-01 16:19:13 -0400

    NLSAR-184 | add COMPUTE_BOX_HOSTNAME and COMPUTE_BOX_USERNAME

--- autorally_util/setupEnvXavierWarthog.sh
+++ autorally_util/setupEnvXavierWarthog.sh
@@ -2,15 +2,20 @@
 
 # source this before you perform a distributed launch with a remote AutoRally chassis
 
-#export ROS_MASTER_URI="http://cpr-lav07:11311"
+# This is the Warthog main computer
+## export ROS_MASTER_URI="http://cpr-lav07:11311"
+COMPUTE_BOX_USERNAME="administrator"
 COMPUTE_BOX_HOSTNAME="cpr-lav07"
-COMPUTE_BOX_USERNAME="snowxavier"
 
-# (Priority) todo:validate command >> $ssh cpr-lav07 ls /dev/
+# (Priority) todo:validate command
+#     Pass: clearpath
+#     $ssh cpr-lav07 ls /dev/
+#     $ssh administrator@cpr-lav07 ls /dev/
+#
 
 export ROS_MASTER_URI=http://${COMPUTE_BOX_HOSTNAME}:11311
-export MASTER_HOSTNAME=${COMPUTE_BOX_HOSTNAME}
 export MASTER_USER=${COMPUTE_BOX_USERNAME}
+export MASTER_HOSTNAME=${COMPUTE_BOX_HOSTNAME}
 export HOSTNAME=$(hostname)
 export ROSLAUNCH_SSH_UNKNOWN=1
 # Find directory of script file to avoid hard-coded paths
