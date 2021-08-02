b2a7a7845afc6699b62ea4e444b3923727223726
RedLeaderOne <luc.coupal.1@ulaval.ca>
2021-08-01 22:10:47 -0400

    NLSAR-202 | linther

--- autorally_control/include/autorally_control/path_integral/autorally_plant.h
+++ autorally_control/include/autorally_control/path_integral/autorally_plant.h
@@ -81,206 +81,210 @@ namespace autorally_control {
 * and status information for both the controller and the OCS.
 */
 
-class AutorallyPlant //: public Diagnostics
-{
-public:
-  static const int AUTORALLY_STATE_DIM = 7;
-  static const int AUTORALLY_CONTROL_DIM = 2;
-  //Struct for holding the autorally pose.
-  typedef struct
-  {
-    //X-Y-Z position
-    float x_pos;
-    float y_pos;
-    float z_pos;
-    //1-2-3 Euler angles
-    float roll;
-    float pitch;
-    float yaw;
-    //Quaternions
-    float q0;
-    float q1;
-    float q2;
-    float q3;
-    //X-Y-Z velocity.
-    float x_vel;
-    float y_vel;
-    float z_vel;
-    //Body frame velocity
-    float u_x;
-    float u_y;
-    //Euler angle derivatives
-    float yaw_mder;
-    //Current servo commands
-    float steering;
-    float throttle;
-  } FullState;
+    class AutorallyPlant //: public Diagnostics
+    {
+    public:
+        static const int AUTORALLY_STATE_DIM = 7;
+        static const int AUTORALLY_CONTROL_DIM = 2;
+        //Struct for holding the autorally pose.
+        typedef struct {
+            //X-Y-Z position
+            float x_pos;
+            float y_pos;
+            float z_pos;
+            //1-2-3 Euler angles
+            float roll;
+            float pitch;
+            float yaw;
+            //Quaternions
+            float q0;
+            float q1;
+            float q2;
+            float q3;
+            //X-Y-Z velocity.
+            float x_vel;
+            float y_vel;
+            float z_vel;
+            //Body frame velocity
+            float u_x;
+            float u_y;
+            //Euler angle derivatives
+            float yaw_mder;
+            //Current servo commands
+            float steering;
+            float throttle;
+        } FullState;
 
-  float last_heading_ = 0.0;
-  float throttleMax_ = 0.99;
-  int heading_multiplier_ = 0;
+        float last_heading_ = 0.0;
+        float throttleMax_ = 0.99;
+        int heading_multiplier_ = 0;
 
-	boost::mutex access_guard_;
-  std::string nodeNamespace_;
+        boost::mutex access_guard_;
+        std::string nodeNamespace_;
 
-  bool new_model_available_;
-  cv::Mat debugImg_;
+        bool new_model_available_;
+        cv::Mat debugImg_;
 
-  bool solutionReceived_ = false;
-  bool is_nodelet_;
-  std::vector<float> controlSequence_;
-  std::vector<float> stateSequence_;
-  util::EigenAlignedVector<float, 2, 7> feedback_gains_;
-  ros::Time solutionTs_;
+        bool solutionReceived_ = false;
+        bool is_nodelet_;
+        std::vector<float> controlSequence_;
+        std::vector<float> stateSequence_;
+        util::EigenAlignedVector<float, 2, 7> feedback_gains_;
+        ros::Time solutionTs_;
 
-  int numTimesteps_;
-  double deltaT_;
+        int numTimesteps_;
+        double deltaT_;
 
-  double optimizationLoopTime_;
+        double optimizationLoopTime_;
 
-  /**
-  * @brief Constructor for AutorallyPlant, takes the a ros node handle and initalizes
-  * publishers and subscribers.
-  * @param mppi_node A ros node handle.
-  */
-	AutorallyPlant(ros::NodeHandle global_node, ros::NodeHandle mppi_node,
-                 bool debug_mode, int hz, bool nodelet);
+        /**
+        * @brief Constructor for AutorallyPlant, takes the a ros node handle and initalizes
+        * publishers and subscribers.
+        * @param mppi_node A ros node handle.
+        */
+        AutorallyPlant(ros::NodeHandle global_node, ros::NodeHandle mppi_node,
+                       bool debug_mode, int hz, bool nodelet);
 
-	AutorallyPlant(ros::NodeHandle global_node, bool debug_mode, int hz):AutorallyPlant(global_node, global_node, debug_mode, hz, false){};
+        AutorallyPlant(ros::NodeHandle global_node, bool debug_mode, int hz) : AutorallyPlant(global_node, global_node,
+                                                                                              debug_mode, hz, false) {};
 
-  /**
-  * @brief Callback for /pose_estimate subscriber.
-  */
-	void poseCall(nav_msgs::Odometry pose_msg);
+        /**
+        * @brief Callback for /pose_estimate subscriber.
+        */
+        void poseCall(nav_msgs::Odometry pose_msg);
 
-  /**
-  * @brief Callback for recording the current servo input.
-  */
-  void servoCall(autorally_msgs::chassisState servo_msg);
+        /**
+        * @brief Callback for recording the current servo input.
+        */
+        void servoCall(autorally_msgs::chassisState servo_msg);
 
-  bool hasNewModel();
-  virtual void modelCall(autorally_msgs::neuralNetModel model_msg);
-  virtual void getModel(std::vector<int> &description, std::vector<float> &data);
+        bool hasNewModel();
 
-  /**
-  * @brief Callback for safe speed subscriber.
-  */
-	void runstopCall(autorally_msgs::runstop safe_msg);
+        virtual void modelCall(autorally_msgs::neuralNetModel model_msg);
 
-  /**
-  * @brief Publishes the controller's nominal path.
-  */
-	void pubPath(const ros::TimerEvent&);
+        virtual void getModel(std::vector<int> &description, std::vector<float> &data);
 
-  void setSolution(std::vector<float> traj, std::vector<float> controls,
-                   util::EigenAlignedVector<float, 2, 7> gains,
-                   ros::Time timestamp, double loop_speed);
+        /**
+        * @brief Callback for safe speed subscriber.
+        */
+        void runstopCall(autorally_msgs::runstop safe_msg);
 
-  void setDebugImage(cv::Mat img);
+        /**
+        * @brief Publishes the controller's nominal path.
+        */
+        void pubPath(const ros::TimerEvent &);
 
-  void setTimingInfo(double poseDiff, double tickTime, double sleepTime);
+        void setSolution(std::vector<float> traj, std::vector<float> controls,
+                         util::EigenAlignedVector<float, 2, 7> gains,
+                         ros::Time timestamp, double loop_speed);
 
-  void pubTimingData(const ros::TimerEvent&);
+        void setDebugImage(cv::Mat img);
 
-  /**
-  * @brief Publishes a control input.
-  * @param steering The steering command to publish.
-  * @param throttle The throttle command to publish.
-  */
-	void pubControl(float steering, float throttle);
+        void setTimingInfo(double poseDiff, double tickTime, double sleepTime);
 
-  void pubStatus(const ros::TimerEvent&);
+        void pubTimingData(const ros::TimerEvent &);
 
-  /**
-  * @brief Returns the current state of the system.
-  */
-	virtual AutorallyPlant::FullState getState();
+        /**
+        * @brief Publishes a control input.
+        * @param steering The steering command to publish.
+        * @param throttle The throttle command to publish.
+        */
+        void pubControl(float steering, float throttle);
 
-  /**
-  * @brief Returns the current value of safe speed.
-  */
-  bool getRunstop();
+        void pubStatus(const ros::TimerEvent &);
 
-  /**
-  * @brief Returns the timestamp of the last pose callback.
-  */
-  ros::Time getLastPoseTime();
+        /**
+        * @brief Returns the current state of the system.
+        */
+        virtual AutorallyPlant::FullState getState();
 
-  /**
-  * @brief Checks the system status.
-  * @return An integer specifying the status. 0 means the system is operating
-  * nominally, 1 means something is wrong but no action needs to be taken,
-  * 2 means that the vehicle should stop immediately.
-  */
-  int checkStatus();
+        /**
+        * @brief Returns the current value of safe speed.
+        */
+        bool getRunstop();
 
-  void dynRcfgCall(autorally_control::PathIntegralParamsConfig &config, int lvl);
+        /**
+        * @brief Returns the timestamp of the last pose callback.
+        */
+        ros::Time getLastPoseTime();
 
-  bool hasNewDynRcfg();
+        /**
+        * @brief Checks the system status.
+        * @return An integer specifying the status. 0 means the system is operating
+        * nominally, 1 means something is wrong but no action needs to be taken,
+        * 2 means that the vehicle should stop immediately.
+        */
+        int checkStatus();
 
-  autorally_control::PathIntegralParamsConfig getDynRcfgParams();
+        void dynRcfgCall(autorally_control::PathIntegralParamsConfig &config, int lvl);
 
-  virtual void displayDebugImage(const ros::TimerEvent&);
+        bool hasNewDynRcfg();
 
-  virtual bool hasNewObstacles(){return false;};
-  virtual void getObstacles(std::vector<int> &description, std::vector<float> &data){};
+        autorally_control::PathIntegralParamsConfig getDynRcfgParams();
 
-  virtual bool hasNewCostmap(){return false;};
-  virtual void getCostmap(std::vector<int> &description, std::vector<float> &data){};
+        virtual void displayDebugImage(const ros::TimerEvent &);
 
-  virtual void shutdown();
+        virtual bool hasNewObstacles() { return false; };
 
-protected:
-  //SystemParams mppiParams_;
-  int poseCount_ = 0;
-  bool useFeedbackGains_ = false;
-  std::atomic<bool> receivedDebugImg_;
-  std::atomic<bool> debugShutdownSignal_;
-  std::atomic<bool> debugShutdownSignalAcknowledged_;
-  autorally_msgs::neuralNetModel dynamicsModel_;
-  autorally_control::PathIntegralParamsConfig costParams_;
-  bool hasNewCostParams_ = false;
+        virtual void getObstacles(std::vector<int> &description, std::vector<float> &data) {};
 
-  const double TIMEOUT = 0.5; ///< Time before declaring pose/controls stale.
+        virtual bool hasNewCostmap() { return false; };
 
-  FullState full_state_; ///< Full state of the autorally vehicle.
+        virtual void getCostmap(std::vector<int> &description, std::vector<float> &data) {};
 
-  int hz_; ///< The frequency of the control publisher.
+        virtual void shutdown();
 
-  int status_; ///< System status
-  std::string ocs_msg_; ///< Message to send to the ocs.
+    protected:
+        //SystemParams mppiParams_;
+        int poseCount_ = 0;
+        bool useFeedbackGains_ = false;
+        std::atomic<bool> receivedDebugImg_;
+        std::atomic<bool> debugShutdownSignal_;
+        std::atomic<bool> debugShutdownSignalAcknowledged_;
+        autorally_msgs::neuralNetModel dynamicsModel_;
+        autorally_control::PathIntegralParamsConfig costParams_;
+        bool hasNewCostParams_ = false;
 
-  bool safe_speed_zero_; ///< Current value of safe speed.
-  bool debug_mode_; ///< Whether or not the system is in debug/simulation mode.
-  bool activated_; ///< Whether or not we've received an initial pose message.
+        const double TIMEOUT = 0.5; ///< Time before declaring pose/controls stale.
 
-  ros::Time last_pose_call_; ///< Timestamp of the last pose callback.
+        FullState full_state_; ///< Full state of the autorally vehicle.
 
-  ros::Publisher control_pub_head_; ///< Publisher of autorally_msgs::mppi_cmd_vel_head type on topic servoCommand (required by AutoRally)
-  ros::Publisher control_pub_; ///< Publisher of geometry_msgs::Twist type on topic servoCommand (required by the Warthog main computer)
-  ros::Publisher status_pub_; ///< Publishes the status (0 good, 1 neutral, 2 bad) of the controller
-  ros::Publisher subscribed_pose_pub_; ///< Publisher of the subscribed pose
-  ros::Publisher path_pub_; ///< Publisher of nav_mags::Path on topic nominalPath.
-  ros::Publisher timing_data_pub_;
-  ros::Subscriber pose_sub_; ///< Subscriber to /pose_estimate.
-  ros::Subscriber servo_sub_;
-  ros::Subscriber model_sub_;
-  ros::Timer pathTimer_;
-  ros::Timer statusTimer_;
-  ros::Timer debugImgTimer_;
-  ros::Timer timingInfoTimer_;
+        int hz_; ///< The frequency of the control publisher.
 
-  nav_msgs::Path path_msg_; ///< Path message for publishing the planned path.
-  geometry_msgs::Point time_delay_msg_; ///< Point message for publishing the observed delay.
-  autorally_msgs::pathIntegralStatus status_msg_; ///<pathIntegralStatus message for publishing mppi status
-  autorally_msgs::pathIntegralTiming timingData_; ///<pathIntegralStatus message for publishing mppi status
+        int status_; ///< System status
+        std::string ocs_msg_; ///< Message to send to the ocs.
+
+        bool safe_speed_zero_; ///< Current value of safe speed.
+        bool debug_mode_; ///< Whether or not the system is in debug/simulation mode.
+        bool activated_; ///< Whether or not we've received an initial pose message.
+
+        ros::Time last_pose_call_; ///< Timestamp of the last pose callback.
+
+        ros::Publisher control_pub_head_; ///< Publisher of autorally_msgs::mppi_cmd_vel_head type on topic servoCommand (required by AutoRally)
+        ros::Publisher control_pub_; ///< Publisher of geometry_msgs::Twist type on topic servoCommand (required by the Warthog main computer)
+        ros::Publisher status_pub_; ///< Publishes the status (0 good, 1 neutral, 2 bad) of the controller
+        ros::Publisher subscribed_pose_pub_; ///< Publisher of the subscribed pose
+        ros::Publisher path_pub_; ///< Publisher of nav_mags::Path on topic nominalPath.
+        ros::Publisher timing_data_pub_;
+        ros::Subscriber pose_sub_; ///< Subscriber to /pose_estimate.
+        ros::Subscriber servo_sub_;
+        ros::Subscriber model_sub_;
+        ros::Timer pathTimer_;
+        ros::Timer statusTimer_;
+        ros::Timer debugImgTimer_;
+        ros::Timer timingInfoTimer_;
+
+        nav_msgs::Path path_msg_; ///< Path message for publishing the planned path.
+        geometry_msgs::Point time_delay_msg_; ///< Point message for publishing the observed delay.
+        autorally_msgs::pathIntegralStatus status_msg_; ///<pathIntegralStatus message for publishing mppi status
+        autorally_msgs::pathIntegralTiming timingData_; ///<pathIntegralStatus message for publishing mppi status
 
 //  // ❯❯❯ SNOW-AutoRally refactoring ❯❯❯.................................................................................
 //  geometry_msgs::Twist mppi_cmd_vel_head_msg_; ///< SNOW-AutoRally Command velocity message .
 //  // .................................................................................❮❮❮ SNOW-AutoRally refactoring ❮❮❮
 
 
-};
+    };
 
 }
 
